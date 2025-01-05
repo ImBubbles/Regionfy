124d6539c1d1bc70bd537d6ee12246c731cce61c
ImBubbles <73860227+ImBubbles@users.noreply.github.com>
2025-01-05 01:38:44 -0600

    Delete src/main/java/me/bubbles/regionfy/utility/chance directory

--- src/main/java/me/bubbles/regionfy/utility/chance/Activation.java
+++
@@ -1,9 +0,0 @@
-package me.bubbles.regionfy.utility.chance;
-
-public class Activation extends Chance<Boolean> {
-
-    public Activation(double min, double max, double below) {
-        super(true, false, min, max, below);
-    }
-
-}
